da9ee25d1efc8c3711a5cd116b12eadf2a21f65f
rforstmane <35078285+rforstmane@users.noreply.github.com>
2018-10-22 23:57:38 +0300

    Added event handling for input labels

--- main.js
+++ main.js
@@ -1,4 +1,22 @@
-function myFunction() {
-    var element = document.getElementById("renda");
-    element.classList.toggle("poga");
+"use strict";
+let inputs =  document.getElementsByClassName("input-group__input")
+console.log(inputs);
+
+for(let i = 0; i < inputs.length; i++) {
+    inputs[i].addEventListener("focus", focusHandler)
+    inputs[i].addEventListener("blur", blurHandler)
+
+    if (inputs[i].value !== "") {
+        inputs[i].parentElement.classList.add("input-group--active")
+    }
+}
+
+function focusHandler(){
+    this.parentElement.classList.add("input-group--active")
+}
+
+function blurHandler(){
+    if (this.value === "") {
+        this.parentElement.classList.remove("input-group--active")
+    }
 }
