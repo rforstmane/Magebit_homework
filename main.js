a7ee016d666e50ba1c0cfa9cbc5af4099a2fe8d9
rforstmane <35078285+rforstmane@users.noreply.github.com>
2018-10-24 23:47:28 +0300

    Added animations

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 "use strict";
 let inputs =  document.getElementsByClassName("input-group__input")
-console.log(inputs);
 
 for(let i = 0; i < inputs.length; i++) {
     inputs[i].addEventListener("focus", focusHandler)
@@ -21,10 +20,14 @@ function blurHandler(){
     }
 }
 
-// $('.login--signup').hide();
-$('.button').on('click',
-  function() {
-    $('.login--login, .form').toggle(2000);
-  }
-);
+$("#js-form-right").on("click", function() {
+    $(".auth-form__modal").addClass("auth-form__modal--login")
+    $(".auth-form__modal").removeClass("auth-form__modal--signup")
 
+})
+
+$("#js-form-left").on("click", function() {
+    $(".auth-form__modal").addClass("auth-form__modal--signup")
+    $(".auth-form__modal").removeClass("auth-form__modal--login")
+
+})
