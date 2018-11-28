07b3d759388d42e2f8ca8a2c88fb7d695e88dd9b
rforstmane <35078285+rforstmane@users.noreply.github.com>
2018-11-28 11:22:31 +0200

    Separated html files in php files

---
+++ login.php
@@ -0,0 +1,32 @@
+<form id="login-form" class="form form--login" method="POST">
+    <div class="form__title">
+        <h2>Login</h2>
+        <img src="images/logo.png" alt="logo">
+    </div>
+    <div class="input-group">
+        <input class="input-group__input" type="email">
+        <label class="input-group__label">
+            <span class="input-group__label-text">Email</span>
+        </label>
+        <svg class="input-group__icon" xmlns="http://www.w3.org/2000/svg" width="16" height="14">
+            <path id="ic_mail"
+                  d="M790 188h16v14h-16v-14zm2 2h12v10h-12v-10zm12.989.352l-6.637 6.637-.352-.352-.352.352-6.637-6.637 1.341-1.341 5.648 5.648 5.648-5.648z"
+                  transform="translate(-790 -188)"/>
+        </svg>
+    </div>
+    <div class="input-group">
+        <input class="input-group__input" type="password">
+        <label class="input-group__label">
+            <span class="input-group__label-text">Password</span>
+        </label>
+        <svg class="input-group__icon" xmlns="http://www.w3.org/2000/svg" width="16" height="17">
+            <path id="ic_lock"
+                  d="M826 169h16v10h-16v-10zm2 2h12v6h-12v-6zm9-1v-3a3 3 0 0 0-6 0v3h-2v-3a5 5 0 0 1 10 0v3h-2z"
+                  transform="translate(-826 -162)"/>
+        </svg>
+    </div>
+    <div class="login-group">
+        <a href="#" class="login-group__button">LOGIN</a>
+        <a href="#" class="login-group__forgot">Forgot?</a>
+    </div>
+</form>
---
+++ main.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8"/>
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700" rel="stylesheet">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Magebit homework</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="style.css"/>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+</head>
+
+<body>
+<div class="wrapper">
+    <div class="auth-form">
+        <div class="auth-form__column">
+            <h2 class="auth-form__title">
+                Don't have an account?
+            </h2>
+            <p class="auth-form__text">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
+                labore et dolore magna aliqua.
+            </p>
+            <a href="#signup-form" class="button" id="js-form-left">SIGN UP</a>
+        </div>
+        <div class="auth-form__column">
+            <div class="auth-form__title">
+                <h2>Have an account?</h2>
+            </div>
+            <div class="auth-form__text">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </div>
+            <a href="#login-form" class="button" id="js-form-right">LOGIN</a>
+        </div>
+        <div class="auth-form__modal auth-form__modal--login ">
+            <?php include('signup.php'); ?>
+            <?php include('login.php'); ?>
+
+
+        </div>
+    </div>
+
+    <script src="main.js"></script>
+    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+</body>
+
+</html>
