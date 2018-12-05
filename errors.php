88c70ca316f83ef7a5dd38c231a53579d4c0e332
rforstmane <35078285+rforstmane@users.noreply.github.com>
2018-12-05 17:01:48 +0200

    Added styles for errors

--- errors.php
+++ errors.php
@@ -1,5 +1,7 @@
-<?php if(count($errors) > 0): ?>
-    <?php foreach ($errors as $error): ?>
-        <p class="error" style="color: red"><?php echo $error; ?></p>
-    <?php endforeach; ?>
+<?php if (count($errors) > 0): ?>
+    <div class="error">
+        <?php foreach ($errors as $error): ?>
+            <p><?php echo $error; ?></p>
+        <?php endforeach; ?>
+    </div>
 <?php endif; ?>
