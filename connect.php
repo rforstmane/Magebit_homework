28a7bc6daf644d47fe9eb9c1a6b8ec6233d2642c
rforstmane <35078285+rforstmane@users.noreply.github.com>
2018-12-09 17:44:49 +0200

    Added updated attributes

--- connect.php
+++ connect.php
@@ -82,7 +82,8 @@ if (isset($_GET['logout'])) {
     header('location: main.php');
 }
 
-function merge_form_input($id_array, $keey_array, $value_array, $db_connect) {
+function merge_form_input($id_array, $keey_array, $value_array, $db_connect)
+{
     $all_inputs_array = [];
     foreach ($id_array as $i => $item_id) {
         $object = new stdClass();
@@ -94,7 +95,8 @@ function merge_form_input($id_array, $keey_array, $value_array, $db_connect) {
     return $all_inputs_array;
 }
 
-function store_new_inputs($inputs_array, $user_id, $db_connect) {
+function store_new_inputs($inputs_array, $user_id, $db_connect)
+{
     $new_inputs = [];
     foreach ($inputs_array as $item) {
         if ($item->id == "") {
@@ -110,14 +112,73 @@ function store_new_inputs($inputs_array, $user_id, $db_connect) {
     }
 }
 
-if (isset($_POST['submit'])) {
+function get_potential_updated($inputs_array)
+{
+    $potential_updated_inputs = [];
+    foreach ($inputs_array as $item) {
+        if ($item->id != "") {
+            array_push($potential_updated_inputs, $item);
+        }
+    }
+    return $potential_updated_inputs;
+}
 
+function join_ids($potential_updated_inputs)
+{
+    $id_array = [];
+    foreach ($potential_updated_inputs as $item) {
+        array_push($id_array, $item->id);
+    }
+
+    $result = join(", ", $id_array);
+    return $result;
+}
+
+function get_old_inputs($potential_updated_inputs, $user_id, $db_connect)
+{
+    $old_inputs_query = "SELECT * FROM attributes WHERE user_id='$user_id' AND id IN (" . join_ids($potential_updated_inputs) . ") ";
+    $old_inputs_result = mysqli_query($db_connect, $old_inputs_query);
+    $old_inputs_rows = mysqli_fetch_all($old_inputs_result, MYSQLI_ASSOC);
+
+    foreach ($old_inputs_rows as $i=>$row) {
+        $old_inputs_rows[$i] = (object)$row;
+    }
+    return $old_inputs_rows;
+}
+
+function find_input_by_id($id, $searchable_array) {
+    foreach ($searchable_array as $item) {
+        if ($item->id == $id) {
+            return $item;
+        }
+    }
+    return null;
+}
+
+function update_existing_inputs($inputs_array, $user_id, $db_connect)
+{
+    $potential_updated_inputs = get_potential_updated($inputs_array);
+    $old_inputs = get_old_inputs($potential_updated_inputs, $user_id, $db_connect);
+
+    foreach ($potential_updated_inputs as $potential_version) {
+        $old_version = find_input_by_id($potential_version->id, $old_inputs);
+
+        if ($old_version != NULL) {
+            if ($old_version->keey != $potential_version->key || $old_version->value != $potential_version->value) {
+                $update_inputs_query = "UPDATE attributes SET keey ='$potential_version->key', value = '$potential_version->value' WHERE id='$potential_version->id'" ;
+                mysqli_query($db_connect, $update_inputs_query);
+            }
+        }
+    }
+}
+
+if (isset($_POST['submit'])) {
     // lokals mainigais, lai vieglak stradat
     $user_id = $_SESSION['user_id'];
     // all inputs submitted form
     $all_inputs_array = merge_form_input($_POST['id'], $_POST['keey'], $_POST['value'], $connect);
     // inputs which have been newly added
-
     store_new_inputs($all_inputs_array, $user_id, $connect);
+    update_existing_inputs($all_inputs_array, $user_id, $connect);
 }
 ?>
