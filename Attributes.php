8a4ba9b943846708b4668ee1fe2198401c915de6
rforstmane <35078285+rforstmane@users.noreply.github.com>
2018-12-30 19:18:19 +0200

    Refactor Attrubutes class

--- Attributes.php
+++ Attributes.php
@@ -21,20 +21,20 @@ class Attributes {
         $this->dbConnection->close();
     }
 
-    function merge_form_input($id_array, $keey_array, $value_array, $db_connect)
+    function merge_form_input($id_array, $keey_array, $value_array)
     {
         $all_inputs_array = [];
         foreach ($id_array as $i => $item_id) {
             $object = new stdClass();
-            $object->id = mysqli_real_escape_string($db_connect, $id_array[$i]);
-            $object->key = mysqli_real_escape_string($db_connect, $keey_array[$i]);
-            $object->value = mysqli_real_escape_string($db_connect, $value_array[$i]);
+            $object->id = $this->dbConnection->escape_string($id_array[$i]);
+            $object->key = $this->dbConnection->escape_string($keey_array[$i]);
+            $object->value = $this->dbConnection->escape_string($value_array[$i]);
             array_push($all_inputs_array, $object);
         }
         return $all_inputs_array;
     }
 
-    function store_new_inputs($inputs_array, $user_id, $db_connect)
+    function store_new_inputs($inputs_array)
     {
         $new_inputs = [];
         foreach ($inputs_array as $item) {
@@ -45,8 +45,8 @@ class Attributes {
 
         foreach ($new_inputs as $input) {
             if (!(empty($input->key) && empty($input->value))) {
-                $mysql = "INSERT INTO attributes (user_id, keey, value) VALUES ('$user_id', '$input->key', '$input->value') ";
-                mysqli_query($db_connect, $mysql);
+                $mysql = "INSERT INTO attributes (user_id, keey, value) VALUES ('$this->user_id', '$input->key', '$input->value') ";
+                $this->dbConnection->query($mysql);
             }
         }
     }
@@ -73,10 +73,10 @@ class Attributes {
         return $result;
     }
 
-    function get_old_inputs($potential_updated_inputs, $user_id, $db_connect)
+    function get_old_inputs($potential_updated_inputs)
     {
-        $old_inputs_query = "SELECT * FROM attributes WHERE user_id='$user_id' AND id IN (" . $this->join_ids($potential_updated_inputs) . ") ";
-        $old_inputs_result = mysqli_query($db_connect, $old_inputs_query);
+        $old_inputs_query = "SELECT * FROM attributes WHERE user_id='$this->user_id' AND id IN (" . $this->join_ids($potential_updated_inputs) . ") ";
+        $old_inputs_result = $this->dbConnection->query($old_inputs_query);
         if ($old_inputs_result != false) {
             $old_inputs_rows = mysqli_fetch_all($old_inputs_result, MYSQLI_ASSOC);
             foreach ($old_inputs_rows as $i => $row) {
@@ -88,10 +88,10 @@ class Attributes {
         }
     }
 
-    function get_all_attributes($user_id, $db_connect)
+    function get_all_attributes()
     {
-        $old_inputs_query = "SELECT * FROM attributes WHERE user_id ='$user_id' ";
-        $old_inputs_result = mysqli_query($db_connect, $old_inputs_query);
+        $old_inputs_query = "SELECT * FROM attributes WHERE user_id ='$this->user_id' ";
+        $old_inputs_result = $this->dbConnection->query($old_inputs_query);
         $old_inputs_rows = mysqli_fetch_all($old_inputs_result, MYSQLI_ASSOC);
 
         foreach ($old_inputs_rows as $i => $row) {
@@ -110,10 +110,10 @@ class Attributes {
         return null;
     }
 
-    public function update_existing_inputs($inputs_array, $user_id, $db_connect)
+    public function update_existing_inputs($inputs_array)
     {
         $potential_updated_inputs = $this->get_potential_updated($inputs_array);
-        $old_inputs = $this->get_old_inputs($potential_updated_inputs, $user_id, $db_connect);
+        $old_inputs = $this->get_old_inputs($potential_updated_inputs, $this->user_id, $this->dbConnection);
 
         foreach ($potential_updated_inputs as $potential_version) {
             $old_version = $this->find_input_by_id($potential_version->id, $old_inputs);
@@ -121,23 +121,23 @@ class Attributes {
             if ($old_version != NULL) {
                 if ($old_version->keey != $potential_version->key || $old_version->value != $potential_version->value) {
                     $update_inputs_query = "UPDATE attributes SET keey ='$potential_version->key', value = '$potential_version->value' WHERE id='$potential_version->id'";
-                    mysqli_query($db_connect, $update_inputs_query);
+                    $this->dbConnection->query($update_inputs_query);
 
                 }
             }
         }
     }
 
-    public function delete_removed_inputs($inputs_array, $user_id, $db_connect)
+    public function delete_removed_inputs($inputs_array)
     {
         $potential_updated_inputs = $this->get_potential_updated($inputs_array);
-        $old_inputs = $this->get_all_attributes($user_id, $db_connect);
+        $old_inputs = $this->get_all_attributes($this->user_id, $this->dbConnection);
 
         foreach ($old_inputs as $old_version) {
             $potential_version = $this->find_input_by_id($old_version->id, $potential_updated_inputs);
             if ($potential_version == NULL) {
-                $deleted_inputs_query = "DELETE FROM attributes WHERE id='$old_version->id' AND user_id = '$user_id'  ";
-                mysqli_query($db_connect, $deleted_inputs_query);
+                $deleted_inputs_query = "DELETE FROM attributes WHERE id='$old_version->id' AND user_id = '$this->user_id'  ";
+                $this->dbConnection->query($deleted_inputs_query);
             }
         }
     }
@@ -145,13 +145,13 @@ class Attributes {
 
     public function submit($post) {
         // lokals mainigais, lai vieglak stradat
-        $user_id = $_SESSION['user_id'];
+        $this->user_id = $_SESSION['user_id'];
         // all inputs submitted form
         $all_inputs_array = $this->merge_form_input($post['id'], $post['keey'], $post['value'], $this->dbConnection);
         // inputs which have been newly added
-        $this->delete_removed_inputs($all_inputs_array, $user_id, $this->dbConnection);
-        $this->store_new_inputs($all_inputs_array, $user_id, $this->dbConnection);
-        $this->update_existing_inputs($all_inputs_array, $user_id, $this->dbConnection);
+        $this->delete_removed_inputs($all_inputs_array, $this->user_id, $this->dbConnection);
+        $this->store_new_inputs($all_inputs_array, $this->user_id, $this->dbConnection);
+        $this->update_existing_inputs($all_inputs_array, $this->user_id, $this->dbConnection);
         $this->app->pushError("Changes saved successfully");
     }
 
